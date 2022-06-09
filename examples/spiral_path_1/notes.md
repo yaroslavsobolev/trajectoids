29d8131ff136718b82139240340d92d6e9db1a6e
yaroslavsobolev <yaroslav.sobolev@phystech.edu>
2022-06-09 14:09:55 +0900

    Make existence tests for three paths.

---
+++ examples/spiral_path_1/notes.md
@@ -0,0 +1,4 @@
+
+
+Minimized mismatch angle = 2.8202552833972458e-06
+Best scale: 0.2585583934537471
--- tests/existence-tests/existence-tests.py
+++ tests/existence-tests/existence-tests.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import mayavi
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, InterpolatedUnivariateSpline
 from matplotlib.collections import LineCollection
 from matplotlib.colors import ListedColormap, BoundaryNorm
 
@@ -102,6 +102,45 @@ def make_brownian_path(Npath = 150, seed=0, travel_length=0.1, end_with_zero=Tru
         ys = ys - xs*(ys[-1] - ys[0])/(xs[-1] - xs[0])
     return np.stack((xs, ys)).T
 
+def make_archimedes_spiral(turns, rate_parameter, npoints, noise_amplitude, end_with_zero=True, seed=0):
+    np.random.seed(seed)
+    phi_max = turns * 2 * np.pi
+    c1 = phi_max / np.sqrt(2*npoints)
+    phis = c1 * np.sqrt(2 * np.arange(0, npoints))
+    noise = noise_amplitude * (2 * np.random.random_sample(npoints) - 1)
+    rs = phis * rate_parameter + noise
+    xs = rs * np.cos(phis)# + noise_amplitude * np.cos(angles_for_noise)
+    ys = rs * np.sin(phis)# + noise_amplitude * np.sin(angles_for_noise)
+    xs = xs - xs[0]
+    ys = ys - ys[0]
+    if end_with_zero:
+        ys = ys - xs*(ys[-1] - ys[0])/(xs[-1] - xs[0])
+    plt.plot(xs, ys)
+    return np.stack((xs, ys)).T
+
+def make_narrow(npoints, shift=0.05, noise_amplitude=0, end_with_zero=True, seed=0):
+    np.random.seed(seed)
+    noise = noise_amplitude * (2 * np.random.random_sample(npoints) - 1)
+    xs = np.linspace(0, 2, int(round(npoints/2)))
+    ys = np.zeros_like(xs)
+    ys[1:-1] += -1*shift
+    xs2 = np.linspace(2, 0, int(round(npoints/2)))
+    ys2 = np.zeros_like(xs2)
+    ys2[1:-1] += shift
+    xs = np.concatenate((xs[:-1], xs2))
+    ys = np.concatenate((ys[:-1], ys2))
+
+    spline_xs = np.array([0, 0.5, 1.2, 1.5, 2])
+    spline_ys = np.array([0, 0.5, 0.3, -0.3, 0])
+    s = interpolate.InterpolatedUnivariateSpline(spline_xs, spline_ys)
+    ys = ys + s(xs)
+    xs = xs - xs[0]
+    ys = ys - ys[0]
+    # if end_with_zero:
+    #     ys = ys - xs*(ys[-1] - ys[0])/(xs[-1] - xs[0])
+    plt.plot(xs, ys)
+    return np.stack((xs, ys)).T
+
 def upsample_path(input_path, by_factor=10):
     old_indices = np.arange(input_path.shape[0])
     max_index = input_path.shape[0]-1
@@ -112,16 +151,29 @@ def upsample_path(input_path, by_factor=10):
     return np.stack((new_xs, new_ys)).T
 
 def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brownian_path_1/figures',
-                              best_scale = 24.810359103416662):
+                              best_scale = 24.810359103416662,
+                              nframes=300,
+                              maxscale = 26,
+                              figsizefactor = 0.85,
+                              circle_center=[-1.7, -0.6],
+                              circlealpha=1
+):
     target_folder='tests/existence-tests/path_folder'
 
     # input_path_single_section = make_random_path(seed=1, amplitude=3, make_ends_horizontal='both', end_with_zero=True)
     if path_type == 'brownian':
         input_path_single_section = make_brownian_path(seed=0, Npath=150, travel_length=0.1)
         input_path_single_section = upsample_path(input_path_single_section, by_factor=5)
+    elif path_type == 'spiral':
+        input_path_single_section = make_archimedes_spiral(turns=5,
+                                                           rate_parameter=0.1,
+                                                           npoints=150, noise_amplitude=0.2)
+        input_path_single_section = upsample_path(input_path_single_section, by_factor=5)
+    elif path_type == 'narrow':
+        input_path_single_section = make_narrow(npoints=150)
+        # input_path_single_section = upsample_path(input_path_single_section, by_factor=20)
 
-        input_path_0 = double_the_path_nosort(input_path_single_section, do_plot=False)
-
+    input_path_0 = double_the_path_nosort(input_path_single_section, do_plot=False)
     # plotting with color along the line
     x = input_path_0[:, 0]
     y = input_path_0[:, 1]
@@ -145,12 +197,12 @@ def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brow
     # plt.plot(input_path_single_section[:, 0], input_path_single_section[:, 1], '-', color=colors)  # , label='Asymmetric')
     # plt.plot(input_path_0[:, 0], input_path_0[:, 1], '-', color='C0')
     # plot circle showing relative diameter of the sphere
-    circle1 = plt.Circle((-1.7, -0.6), 1/best_scale, color='C1')
+    circle1 = plt.Circle((circle_center[0], circle_center[1]), 1/best_scale, fill=False, linewidth=2, edgecolor='C1', alpha=circlealpha)
     axs.add_patch(circle1)
     plt.axis('equal')
     # cbar = fig.colorbar(line, ax=axs)
     # cbar.set_label('Distance along the period')
-    fig.savefig(path_for_figs + '/brownian_path.png', dpi=300)
+    fig.savefig(path_for_figs + f'/{path_type}_path.png', dpi=300)
     plt.show()
 
     # upsampled_path = upsample_path(input_path_0)
@@ -187,20 +239,18 @@ def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brow
                     tube_radius=tube_radius)
 
     mlab.show()
-
-    nframes = 300
-    maxscale = 26
-    figsizefactor = 0.85
+    minscale=0.01
     fig, axarr = plt.subplots(2, 1, sharex=True, figsize=(7*figsizefactor, 5*figsizefactor))
     # sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_single_section, minscale=24.5, maxscale=25.1)
     # sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_0, minscale=24, maxscale=25)
-    sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_0, minscale=0.01, maxscale=maxscale, nframes=nframes)
+    sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_0, minscale=minscale, maxscale=maxscale, nframes=nframes)
     solution_scale = best_scale
-    ii = np.searchsorted(sweeped_scales, solution_scale)
-    sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
-    mismatch_angles = np.insert(mismatch_angles, ii, 0)
+    # ii = np.searchsorted(sweeped_scales, solution_scale)
+    # sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
+    # mismatch_angles = np.insert(mismatch_angles, ii, 0)
     ax = axarr[0]
     ax.plot(sweeped_scales, np.abs(mismatch_angles)/np.pi*180)
+    # ax.plot(sweeped_scales, mismatch_angles / np.pi * 180)
     ax.axhline(y=0, color='black')
     ax.scatter([solution_scale], [0], s=20, color='red')
     ax.set_yticks(np.arange(0, 181, 20))
@@ -210,11 +260,11 @@ def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brow
     # plt.plot(sweeped_scales, mismatch_angles)
 
     ax = axarr[1]
-    sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_single_section, minscale=0.01, maxscale=maxscale, nframes=nframes)
+    sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_single_section, minscale=minscale, maxscale=maxscale, nframes=nframes)
     solution_scale = best_scale
-    ii = np.searchsorted(sweeped_scales, solution_scale)
-    sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
-    mismatch_angles = np.insert(mismatch_angles, ii, np.pi)
+    # ii = np.searchsorted(sweeped_scales, solution_scale)
+    # sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
+    # mismatch_angles = np.insert(mismatch_angles, ii, np.pi)
     ax.plot(sweeped_scales, np.abs(mismatch_angles))
     # ax.axhline(y=0, color='black')
     ax.scatter([solution_scale], [np.pi], s=20, color='red')
@@ -229,9 +279,13 @@ def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brow
 
 
     do_plot = True
-    npoints = 30
 
-    best_scale = minimize_mismatch_by_scaling(input_path_0, scale_range=(24.7, 24.9))
+    if path_type == 'brownian':
+        best_scale = minimize_mismatch_by_scaling(input_path_0, scale_range=(24.7, 24.9))
+    elif path_type == 'spiral':
+        best_scale = minimize_mismatch_by_scaling(input_path_0, scale_range=(0.25, 0.27))
+    elif path_type == 'narrow':
+        best_scale = minimize_mismatch_by_scaling(input_path_0, scale_range=(79.3, 79.4))
     print(f'Best scale: {best_scale}')
     # Minimized mismatch angle = -2.6439127092433114e-05
     # Best scale: 0.6387022944333781
@@ -257,9 +311,33 @@ def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brow
         mlab.show()
 
 if __name__ == '__main__':
-    test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brownian_path_1/figures',
-                              best_scale = 24.810359103416662)
+    # test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brownian_path_1/figures',
+    #                           best_scale = 24.810359103416662,
+    #                           nframes=300,
+    #                           maxscale=26,
+    #                           figsizefactor=0.85,
+    #                           circle_center=[-1.7, -0.6],
+    #                           circlealpha=1
+    #                           )
 
+    # test_trajectoid_existence(path_type='spiral', path_for_figs='examples/spiral_path_1/figures',
+    #                           best_scale = 0.2588162519798698,
+    #                           nframes=300,
+    #                           maxscale=0.4,
+    #                           figsizefactor=0.85,
+    #                           circle_center=[0, 0],
+    #                           circlealpha=0.5
+    #                           )
+
+
+    test_trajectoid_existence(path_type='narrow', path_for_figs='examples/narrow_1/figures',
+                              best_scale = 79.35082181975892,
+                              nframes=900,
+                              maxscale=85,
+                              figsizefactor=0.85,
+                              circle_center=[0.75, 0],
+                              circlealpha=1
+                              )
 # sweeped_scales, mismatch_angles = mismatches_for_all_scales()
 # plt.plot(sweeped_scales, np.abs(mismatch_angles))
 # plt.show()
