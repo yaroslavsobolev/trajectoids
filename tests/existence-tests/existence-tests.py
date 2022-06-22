c562930dec62692e00aa6ddd8d3ba00e12ec2409
yaroslavsobolev <yaroslav.sobolev@phystech.edu>
2022-06-22 19:51:39 +0900

    Refactor existence tests

--- tests/existence-tests/existence-tests.py
+++ tests/existence-tests/existence-tests.py
@@ -92,15 +92,36 @@ def mismatches_for_all_scales(input_path, minscale=0.01, maxscale=2, nframes = 1
     return sweeped_scales, np.array(mismatch_angles)
 
 def gb_areas_for_all_scales(input_path, minscale=0.01, maxscale=2, nframes = 100):
-    '''This function does not take into account the possibly changing rotation index of the spherical trace.
-    It has to be accounted for in the downstream code.'''
+    '''This function takes into account the possibly changing rotation index of the spherical trace.'''
     gauss_bonnet_areas = []
     sweeped_scales = np.linspace(minscale, maxscale, nframes)
     for frame_id, scale in enumerate(sweeped_scales):
         print(f'Computing GB_area for scale {scale}')
         input_path_scaled = input_path * scale
         gauss_bonnet_areas.append(get_gb_area(input_path_scaled))
-    return sweeped_scales, np.array(gauss_bonnet_areas)
+
+    gb_areas = np.array(gauss_bonnet_areas)
+    # compensation for integer number of 2*pi due to rotation index of the curve
+    gb_area_zero = round(gb_areas[0]/np.pi) * np.pi
+    gb_areas -= gb_area_zero
+
+    # correct for changes of rotation index I upon scaling. Upon +1 or -1 change of I, the integral of geodesic curvature
+    # (total change of direction) increments or decrements by 2*pi
+    additional_rotation_indices = np.zeros_like(gb_areas)
+    additional_rotation_index_here = 0
+    threshold_for_ind = 2*np.pi*0.75
+    for i in range(1, gb_areas.shape[0]):
+        diff_here = gb_areas[i] - gb_areas[i-1]
+        if np.abs(diff_here) > threshold_for_ind:
+            additional_rotation_index_here += np.round(diff_here/(2*np.pi))
+        additional_rotation_indices[i] = additional_rotation_index_here
+    gb_areas -= 2 * np.pi * additional_rotation_indices
+
+    # # Plot additional rotation indices for debugging
+    # plt.plot(sweeped_scales, additional_rotation_indices, 'o-')
+    # plt.show()
+
+    return sweeped_scales, gb_areas
 
 def make_brownian_path(Npath = 150, seed=0, travel_length=0.1, end_with_zero=True):
     np.random.seed(seed)
@@ -239,6 +260,100 @@ def make_zigzag2(a):
 #     ys = input_path[:, 1]
 #     return input_path, np.array(tips)
 
+def length_along_the_path(input_path_0):
+    x = input_path_0[:, 0]
+    y = input_path_0[:, 1]
+    length_from_start_to_here = np.cumsum( np.sqrt((np.diff(x)**2 + np.diff(y)**2)) )
+    length_from_start_to_here = np.insert(length_from_start_to_here, 0, 0)
+    length_from_start_to_here = np.remainder(length_from_start_to_here, np.max(length_from_start_to_here)/2)
+    length_from_start_to_here /= np.max(length_from_start_to_here)
+    return length_from_start_to_here
+
+def plot_flat_path_with_color(input_path_0, input_path_single_section, axs):
+    '''plotting with color along the line'''
+    length_from_start_to_here = length_along_the_path(input_path_0)
+    x = input_path_0[:, 0]
+    y = input_path_0[:, 1]
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    # Coloring the curve
+    norm = plt.Normalize(length_from_start_to_here.min(), length_from_start_to_here.max())
+    lc = LineCollection(segments, cmap='viridis', norm=norm)
+    lc.set_array(length_from_start_to_here)
+    lc.set_linewidth(1)
+    line = axs.add_collection(lc)
+
+    #black dots at middle and ends of the path
+    for point in [input_path_single_section[0], input_path_single_section[-1], input_path_0[-1]]:
+        axs.scatter(point[0], point[1], color='black', s=10)
+
+    plt.axis('equal')
+
+def plot_spherical_trace_with_color_along_the_trace(input_path_0, input_path_single_section, preliminary_best_scale,
+                                                    verbose=False):
+    length_from_start_to_here = length_along_the_path(input_path_0)
+    plotting_upsample_factor = 1
+    t0 = time.time()
+    sphere_trace = trace_on_sphere(upsample_path(preliminary_best_scale * input_path_0, by_factor=plotting_upsample_factor),
+                                   kx=1, ky=1)
+    sphere_trace_single_section = trace_on_sphere(upsample_path(input_path_single_section * preliminary_best_scale,
+                                                                by_factor=plotting_upsample_factor),
+                                                  kx=1, ky=1)
+    if verbose:
+        print(f'Seconds passed: {time.time() - t0:.3f}')
+        print('Mlab plot begins...')
+    core_radius = 1
+    last_index = sphere_trace.shape[0] // 2
+    mlab.figure(size=(1024, 768), \
+                bgcolor=(1, 1, 1), fgcolor=(0.5, 0.5, 0.5))
+    tube_radius = 0.01
+    plot_sphere(r0=core_radius - tube_radius, line_radius=tube_radius / 4)
+    mlab.plot3d(sphere_trace[:, 0],
+                sphere_trace[:, 1],
+                sphere_trace[:, 2],
+                length_from_start_to_here, colormap='viridis',
+                tube_radius=tube_radius)
+
+def plot_mismatches_vs_scale(ax, input_path_0, sweeped_scales, mismatch_angles, plot_solution, solution_scale):
+    ## This code injects the sampled point at the solution_scale. Otherwise the root can be not at one of sampled points
+    if plot_solution:
+        ii = np.searchsorted(sweeped_scales, solution_scale)
+        sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
+        # value_at_scale = 0
+        value_at_scale = mismatch_angle_for_path(input_path_0 * solution_scale, recursive=False, use_cache=False)
+        mismatch_angles = np.insert(mismatch_angles, ii, value_at_scale)
+    ax.plot(sweeped_scales, np.abs(mismatch_angles)/np.pi*180)
+    # ax.plot(sweeped_scales, mismatch_angles / np.pi * 180)
+    ax.axhline(y=0, color='black')
+    if plot_solution:
+        value_at_scale = interp1d(sweeped_scales, mismatch_angles)(solution_scale)
+        ax.scatter([solution_scale], [value_at_scale], s=20, color='red')
+    ax.set_yticks(np.arange(0, 181, 20))
+    ax.set_ylim(-5, 181)
+    ax.set_ylabel('Mismatch angle (deg.)\nbetween initial and\nfinal orientations after\npassing two periods')
+
+def plot_gb_ares(ax, sweeped_scales, gb_areas, plot_solution, solution_scale):
+
+    ## This code injects the sampled point at the solution_scale. Otherwise the root can be not at one of sampled points
+    # ii = np.searchsorted(sweeped_scales, solution_scale)
+    # sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
+    # mismatch_angles = np.insert(mismatch_angles, ii, np.pi * np.sign(interp1d(sweeped_scales, gb_areas)(solution_scale)))
+
+    ax.plot(sweeped_scales, gb_areas)
+    ax.axhline(y=np.pi, color='black', alpha=0.5)
+    ax.axhline(y=0, color='black', alpha=0.3)
+    ax.axhline(y=-1 * np.pi, color='black', alpha=0.5)
+    if plot_solution:
+        # ax.scatter([solution_scale], [np.pi * np.sign(interp1d(sweeped_scales, gb_areas)(solution_scale))], s=20, color='red')
+        value_at_scale = interp1d(sweeped_scales, gb_areas)(solution_scale)
+        ax.scatter([solution_scale], [value_at_scale], s=20, color='red')
+    ax.set_yticks([-2 * np.pi, -np.pi, 0, np.pi, 2 * np.pi])
+    ax.set_yticklabels(['-2π', '-π', '0', 'π', '2π'])
+    ax.set_ylim(-np.pi * 2 * 1.01, np.pi * 2 * 1.01)
+    ax.set_ylabel('Spherical area $S(\sigma)$\n enclosed by the \nfirst period\'s trace')
+    ax.set_xlabel('Path\'s scale factor $\sigma$ for fixed ball radius $r=1$\n(or $1/r$ for fixed $\sigma=1$)')
+
 def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brownian_path_1/figures',
                               preliminary_best_scale = 24.810359103416662,
                               nframes=300,
@@ -299,156 +414,56 @@ def test_trajectoid_existence(path_type='brownian', path_for_figs='examples/brow
         input_path_single_section, tips = make_zigzag2(np.pi / 2)
 
     input_path_0 = double_the_path_nosort(input_path_single_section, do_plot=False)
-    # plotting with color along the line
-    x = input_path_0[:, 0]
-    y = input_path_0[:, 1]
-    length_from_start_to_here = np.cumsum( np.sqrt((np.diff(x)**2 + np.diff(y)**2)) )
-    length_from_start_to_here = np.insert(length_from_start_to_here, 0, 0)
-    length_from_start_to_here = np.remainder(length_from_start_to_here, np.max(length_from_start_to_here)/2)
-    length_from_start_to_here /= np.max(length_from_start_to_here)
-    points = np.array([x, y]).T.reshape(-1, 1, 2)
-    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    # Plot flat path with color along the path
     fig, axs = plt.subplots()
-
-    # Coloring the curve
-    norm = plt.Normalize(length_from_start_to_here.min(), length_from_start_to_here.max())
-    lc = LineCollection(segments, cmap='viridis', norm=norm)
-    lc.set_array(length_from_start_to_here)
-    lc.set_linewidth(1)
-    line = axs.add_collection(lc)
-
-    for point in [input_path_single_section[0], input_path_single_section[-1], input_path_0[-1]]:
-        plt.scatter(point[0], point[1], color='black', s=10)
-
+    plot_flat_path_with_color(input_path_0, input_path_single_section, axs)
     # plot circle showing relative diameter of the sphere
     if plot_solution:
-        circle1 = plt.Circle((circle_center[0], circle_center[1]), 1/preliminary_best_scale, fill=False, linewidth=2, edgecolor='C1', alpha=circlealpha)
+        circle1 = plt.Circle((circle_center[0], circle_center[1]),
+                             1/preliminary_best_scale, fill=False, linewidth=2, edgecolor='C1', alpha=circlealpha)
         axs.add_patch(circle1)
-    plt.axis('equal')
-
     ## Add colorbar for figures in the paper
     # cbar = fig.colorbar(line, ax=axs)
     # cbar.set_label('Distance along the period')
-
     fig.savefig(path_for_figs + f'/{path_type}_path.png', dpi=300)
     plt.show()
 
-    plotting_upsample_factor = 1
-    t0 = time.time()
-    sphere_trace = trace_on_sphere(upsample_path(preliminary_best_scale * input_path_0, by_factor=plotting_upsample_factor),
-                                   kx=1, ky=1)
-    sphere_trace_single_section = trace_on_sphere(upsample_path(input_path_single_section * preliminary_best_scale,
-                                                                by_factor=plotting_upsample_factor),
-                                                  kx=1, ky=1)
-    print(f'Seconds passed: {time.time() - t0:.3f}')
-    print('Mlab plot begins...')
-    core_radius = 1
-    last_index = sphere_trace.shape[0] // 2
-    mlab.figure(size=(1024, 768), \
-                bgcolor=(1, 1, 1), fgcolor=(0.5, 0.5, 0.5))
-    tube_radius = 0.01
-    plot_sphere(r0=core_radius - tube_radius, line_radius=tube_radius / 4)
-    # l = mlab.plot3d(sphere_trace[last_index:, 0],
-    #                 sphere_trace[last_index:, 1],
-    #                 sphere_trace[last_index:, 2], color=(0, 0, 1),
-    #                 tube_radius=tube_radius)
-    # l = mlab.plot3d(sphere_trace_single_section[:, 0],
-    #                 sphere_trace_single_section[:, 1],
-    #                 sphere_trace_single_section[:, 2], color=(0, 1, 0),
-    #                 tube_radius=tube_radius)
-    l = mlab.plot3d(sphere_trace[:, 0],
-                    sphere_trace[:, 1],
-                    sphere_trace[:, 2],
-                    length_from_start_to_here, colormap='viridis',
-                    tube_radius=tube_radius)
-
+    plot_spherical_trace_with_color_along_the_trace(input_path_0, input_path_single_section, preliminary_best_scale)
     mlab.show()
-    fig, axarr = plt.subplots(2, 1, sharex=True, figsize=(7*figsizefactor, 5*figsizefactor))
-    # sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_single_section, minscale=24.5, maxscale=25.1)
-    # sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_0, minscale=24, maxscale=25)
+
     sweeped_scales, mismatch_angles = mismatches_for_all_scales(input_path_0, minscale=minscale, maxscale=maxscale, nframes=nframes)
-    solution_scale = preliminary_best_scale
-    if plot_solution:
-        ## This code injects the sampled point at the solution_scale. Otherwise the root can be not at one of sampled points
-        ii = np.searchsorted(sweeped_scales, solution_scale)
-        sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
-        mismatch_angles = np.insert(mismatch_angles, ii, 0)
-    ax = axarr[0]
-    ax.plot(sweeped_scales, np.abs(mismatch_angles)/np.pi*180)
-    # ax.plot(sweeped_scales, mismatch_angles / np.pi * 180)
-    ax.axhline(y=0, color='black')
-    if plot_solution:
-        ax.scatter([solution_scale], [0], s=20, color='red')
-    ax.set_yticks(np.arange(0, 181, 20))
-    ax.set_ylim(-5, 181)
-    ax.set_ylabel('Mismatch angle (deg.)\nbetween initial and\nfinal orientations after\npassing two periods')
-
-    ax = axarr[1]
     sweeped_scales, gb_areas = gb_areas_for_all_scales(input_path_single_section, minscale=minscale, maxscale=maxscale, nframes=nframes)
-    # compensation for integer number of 2*pi due to rotation index of the curve
-    gb_area_zero = round(gb_areas[0]/np.pi) * np.pi
-    gb_areas -= gb_area_zero
 
-    # correct for changes of rotation index I upon scaling. Upon +1 or -1 change of I, the integral of geodesic curvature
-    # (total change of direction) increments or decrements by 2*pi
-    additional_rotation_indices = np.zeros_like(gb_areas)
-    additional_rotation_index_here = 0
-    threshold_for_ind = 2*np.pi*0.75
-    for i in range(1, gb_areas.shape[0]):
-        diff_here = gb_areas[i] - gb_areas[i-1]
-        if np.abs(diff_here) > threshold_for_ind:
-            additional_rotation_index_here += np.round(diff_here/(2*np.pi))
-        additional_rotation_indices[i] = additional_rotation_index_here
-
-    gb_areas -= 2 * np.pi * additional_rotation_indices
-
-    solution_scale = preliminary_best_scale
-
-    ## This code injects the sampled point at the solution_scale. Otherwise the root can be not at one of sampled points
-    # ii = np.searchsorted(sweeped_scales, solution_scale)
-    # sweeped_scales = np.insert(sweeped_scales, ii, solution_scale)
-    # mismatch_angles = np.insert(mismatch_angles, ii, np.pi * np.sign(interp1d(sweeped_scales, gb_areas)(solution_scale)))
-
-    ax.plot(sweeped_scales, gb_areas)
-    ax.axhline(y=np.pi, color='black', alpha=0.5)
-    ax.axhline(y=0, color='black', alpha=0.3)
-    ax.axhline(y=-1*np.pi, color='black', alpha=0.5)
-    if plot_solution:
-        ax.scatter([solution_scale], [np.pi * np.sign(interp1d(sweeped_scales, gb_areas)(solution_scale))], s=20, color='red')
-    ax.set_yticks([-2*np.pi, -np.pi, 0, np.pi, 2*np.pi])
-    ax.set_yticklabels(['-2π', '-π', '0', 'π', '2π'])
-    ax.set_ylim(-np.pi*2*1.01, np.pi*2*1.01)
-    ax.set_ylabel('Spherical area $S(\sigma)$\n enclosed by the \nfirst period\'s trace')
-    ax.set_xlabel('Path\'s scale factor $\sigma$ for fixed ball radius $r=1$\n(or $1/r$ for fixed $\sigma=1$)')
+    fig, axarr = plt.subplots(2, 1, sharex=True, figsize=(7*figsizefactor, 5*figsizefactor))
+    plot_mismatches_vs_scale(axarr[0], input_path_0, sweeped_scales, mismatch_angles, plot_solution, preliminary_best_scale)
+    plot_gb_ares(axarr[1], sweeped_scales, gb_areas, plot_solution, preliminary_best_scale)
     plt.tight_layout()
     fig.savefig(path_for_figs + '/angle-vs-scale.png', dpi=300)
-    plt.show()
-
-    plt.plot(sweeped_scales, additional_rotation_indices, 'o-')
     plt.show()
 
     best_scale = minimize_mismatch_by_scaling(input_path_0, scale_range=range_for_searching_the_roots)
     print(f'Best scale: {best_scale}')
 
-    # Plot the spherical trace for the true solution (best_scale found by the root finding)
-    input_path = best_scale * input_path_0
-    sphere_trace = trace_on_sphere(upsample_path(input_path, by_factor=10),
-                                   kx=1, ky=1)
-    sphere_trace_single_section = trace_on_sphere(upsample_path(input_path_single_section * best_scale, by_factor=10),
-                                                  kx=1, ky=1)
-    if do_plot:
-        core_radius = 1
-        mlab.figure(size=(1024, 768), \
-                    bgcolor=(1, 1, 1), fgcolor=(0.5, 0.5, 0.5))
-        tube_radius = 0.01
-        plot_sphere(r0=core_radius - tube_radius, line_radius=tube_radius / 4)
-        l = mlab.plot3d(sphere_trace[:, 0], sphere_trace[:, 1], sphere_trace[:, 2], color=(0, 0, 1),
-                        tube_radius=tube_radius)
-        l = mlab.plot3d(sphere_trace_single_section[:, 0],
-                        sphere_trace_single_section[:, 1],
-                        sphere_trace_single_section[:, 2], color=(0, 1, 0),
-                        tube_radius=tube_radius)
-        mlab.show()
+    # # Plot the spherical trace for the true solution (best_scale found by the root finding)
+    # input_path = best_scale * input_path_0
+    # sphere_trace = trace_on_sphere(upsample_path(input_path, by_factor=10),
+    #                                kx=1, ky=1)
+    # sphere_trace_single_section = trace_on_sphere(upsample_path(input_path_single_section * best_scale, by_factor=10),
+    #                                               kx=1, ky=1)
+    # if do_plot:
+    #     core_radius = 1
+    #     mlab.figure(size=(1024, 768), \
+    #                 bgcolor=(1, 1, 1), fgcolor=(0.5, 0.5, 0.5))
+    #     tube_radius = 0.01
+    #     plot_sphere(r0=core_radius - tube_radius, line_radius=tube_radius / 4)
+    #     l = mlab.plot3d(sphere_trace[:, 0], sphere_trace[:, 1], sphere_trace[:, 2], color=(0, 0, 1),
+    #                     tube_radius=tube_radius)
+    #     l = mlab.plot3d(sphere_trace_single_section[:, 0],
+    #                     sphere_trace_single_section[:, 1],
+    #                     sphere_trace_single_section[:, 2], color=(0, 1, 0),
+    #                     tube_radius=tube_radius)
+    #     mlab.show()
 
 if __name__ == '__main__':
     # UNCOMMENT NEEDED PARTS BELOW TO TEST TWO-PERIOD TRAJECTOID EXISTENCE FOR VARIOUS PATHS
@@ -508,7 +523,7 @@ if __name__ == '__main__':
                               nframes=150,
                               maxscale=15,
                               figsizefactor=0.85,
-                              circle_center=[-1.7, -0.6],
+                              circle_center=[0.6, -0.3],
                               circlealpha=1,
                               range_for_searching_the_roots=(3.9, 4.1)
                               )
